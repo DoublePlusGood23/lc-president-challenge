f16ac3bfc01ee6f456210dbfc373538c1e1babc6
Christopher Smith <chris@hichris.com>
2016-05-26 19:48:49 -0400

    Stuff.

--- chris/fizzbuzz.groovy
+++ chris/fizzbuzz.groovy
@@ -1,6 +1,9 @@
-for(int i = 0; i <= 100; i++){
-	if(i % 3 == 0 && i % 2 == 0) print 'FizzBuzz';
-	else if(i % 3 == 0) print 'Fizz';
-	else if(i % 2 == 0) print 'Buzz';
-	else print i;
+int index = System.console().readLine('Enter index: ') as Integer
+
+for(int i = 1; i <= 100; i++){
+	if(index != i) continue
+	if(i % 3 == 0 && i % 5 == 0) print 'FizzBuzz'
+	else if(i % 3 == 0) print 'Fizz'
+	else if(i % 5 == 0) print 'Buzz'
+	else print i
 }
