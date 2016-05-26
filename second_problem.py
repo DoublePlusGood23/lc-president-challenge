0d01befb1223ef13a135bc483e534c35e7e9eac0
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2016-05-26 15:28:55 -0400

    Fixed up some stuff

--- second_problem.py
+++ second_problem.py
@@ -1,10 +1,11 @@
-letters = 'bcdfghjklmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ'
+not_vowel = 'bcdfghjklmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ'
 output = ''
 
 phrase = input()
 
 for char in phrase:
-    if char in letters:
+    if char in not_vowel:
+        # Assumes there is no capital letters inside of the word
         output += char + 'o' + char.lower()
     else:
         output += char
