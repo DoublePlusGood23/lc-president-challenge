8f280cece4d59e36ebfeb5486f25c7ac92718c13
Alex D. Zharichenko <alex.zharichenko@quantumcode.io>
2016-05-26 15:33:21 -0400

    Clean it up a bit

--- third_problem.py
+++ third_problem.py
@@ -1,16 +1,18 @@
-letters = 'bcdfghjklmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ'
+not_vowel = 'bcdfghjklmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ'
 
 phrase = input()
+
 output = ''
 vowels = ''
 
+# Remove sapces
 phrase = phrase.replace(' ', '')
 
 for char in phrase:
-    if char in letters:
-        output += char
+    if char in not_vowel:
+        output += char  # Add non vowel to output
     else:
-        vowels += char
+        vowels += char  # Add vowels to vowels
 
 print(output)
 print(vowels)
