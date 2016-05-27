6205dc82b2ddc9c9cefc68143a94f14831e85801
Christopher Smith <chris@hichris.com>
2016-05-26 20:27:32 -0400

    Stuff.

--- chris/rovarspraket.groovy
+++ chris/rovarspraket.groovy
@@ -1,13 +1,15 @@
 List<String> vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'i']
 
-String input = new File('../wap.txt').getText()
-String output = ''
+String input = new File('../results/wap.txt').getText()
+StringBuilder output = new StringBuilder()
 
-for(String c : input){
-	if(!vowels.contains(c)){
-		output += '' + c + 'o' + Character.toLowerCase(c.charAt(0))
+for(char c : input.toCharArray()){
+	if(!vowels.contains(c as String)){
+		output.append(c)
+		output.append('o')
+		output.append(Character.toLowerCase(c))
 	} else {
-		output += c
+		output.append(c)
 	}
 }
 
