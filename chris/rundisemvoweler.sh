9f2ced25a6715fe10c8aa5800ff4569e4bbbad0c
Ethan R. Jones <doubleplusgood23@gmail.com>
2016-05-27 23:38:30 -0400

    Restructured run script.

--- chris/rundisemvoweler.sh
+++ chris/rundisemvoweler.sh
@@ -1,1 +1,5 @@
-nano -c disemvoweler.asm && nasm -f elf32 disemvoweler.asm -o disemvoweler.o && ld -melf_i386 -s -o disemvoweler disemvoweler.o && chmod +x disemvoweler && ./disemvoweler
+nano -c disemvoweler.asm && \
+nasm -f elf32 disemvoweler.asm -o disemvoweler.o && \
+ld -melf_i386 -s -o disemvoweler.bin disemvoweler.o && \
+chmod +x disemvoweler.bin && \
+./disemvoweler.bin
