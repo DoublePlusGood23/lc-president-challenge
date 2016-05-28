da3f95d1e05d435cca8c1af01d058ae2180d345e
Ethan R. Jones <doubleplusgood23@gmail.com>
2016-05-27 23:56:38 -0400

    Switch to elf naming.

--- chris/rundisemvoweler.sh
+++ chris/rundisemvoweler.sh
@@ -1,5 +1,4 @@
-nano -c disemvoweler.asm && \
-nasm -f elf32 disemvoweler.asm -o disemvoweler.o && \
-ld -melf_i386 -s -o disemvoweler.bin disemvoweler.o && \
-chmod +x disemvoweler.bin && \
-./disemvoweler.bin
+nasm -f elf32 -F dwarf -g disemvoweler.asm -o disemvoweler.o && \
+ld -melf_i386 -s -o disemvoweler.elf disemvoweler.o && \
+chmod +x disemvoweler.elf && \
+./disemvoweler.elf
